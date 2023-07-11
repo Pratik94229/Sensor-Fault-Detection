faca030b4a027356da7213be9ed5dff4fc2170cb
Pratik94229 <pratik.941@gmail.com>
2023-07-11 23:51:38 +0530

    model framework created

---
+++ sensor/configuration/mongo_db_connection.py
@@ -0,0 +1,26 @@
+import pymongo
+from sensor.constant.database import DATABASE_NAME
+from sensor.constant.env_variable import MONGODB_URL_KEY
+import certifi
+import os
+ca = certifi.where()
+
+class MongoDBClient:
+    client = None
+    def __init__(self, database_name=DATABASE_NAME) -> None:
+        try:
+
+            if MongoDBClient.client is None:
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                print(mongo_db_url)
+                if "localhost" in mongo_db_url:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
+                else:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise e
+
+
---
+++ sensor/constant/s3_bucket.py
@@ -0,0 +1,2 @@
+TRAINING_BUCKET_NAME = "scania-sensor-pipeline"
+PREDICTION_BUCKET_NAME = "sensor-datasource"
